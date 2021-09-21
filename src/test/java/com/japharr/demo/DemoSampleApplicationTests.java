c01db5e38f3fdcc3baa6f7fcb67e60909d898309
Jelil Adesina <jtutoff@gmail.com>
2021-09-21 15:51:11 +0100

    initial commit

---
+++ src/test/java/com/japharr/demo/DemoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japharr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
